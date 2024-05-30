a767bc140a5f1bd7baf3c22512eff3c5f56b3250
gerti1991 <gskerja@gmail.com>
2024-05-30 11:56:15 +0200

    last

--- lib/app.ts
+++ lib/app.ts
@@ -2,16 +2,18 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/routes";
 import * as mongoose from "mongoose";
-
 import * as fs from 'fs';
 import * as cron from "node-cron"; // Import the cron library
 import * as childProcess from "child_process"; // Import child_process module
+const dotenv = require('dotenv').config()
+
+
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
-    public mongoUrl: string = 'mongodb://localhost/Events';
+    public mongoUrl: string = process.env.MONGO_URI || 'mongodb://localhost/Events';
 
     constructor() {
         this.app = express();
@@ -36,6 +38,7 @@ class App {
                 console.error("MongoDB connection error:", error);
             });
     }
+
     private scheduleCronJob(): void {
         cron.schedule('29 21  * * *', () => {
             const lockFilePath = './EventDates.lock';
@@ -50,10 +53,10 @@ class App {
             fs.writeFileSync(lockFilePath, '');
 
             console.log('Running EventDates.js script...');
-            // Execute the results_basketball.js script using child_process.spawn
+            // Execute the EventDates.js script using child_process.spawn
             const scraperProcess = childProcess.spawn('node', ['./lib/cron/EventDates.js']);
 
-            // Log stdout and stderr from the results_basketball.js script
+            // Log stdout and stderr from the EventDates.js script
             scraperProcess.stdout.on('data', (data) => {
                 console.log(`stdout: ${data}`);
             });
@@ -82,10 +85,10 @@ class App {
             fs.writeFileSync(lockFilePath, '');
 
             console.log('Running EventLinks.js script...');
-            // Execute the results_basketball.js script using child_process.spawn
+            // Execute the EventLinks.js script using child_process.spawn
             const scraperProcess = childProcess.spawn('node', ['./lib/cron/EventLinks.js']);
 
-            // Log stdout and stderr from the results_basketball.js script
+            // Log stdout and stderr from the EventLinks.js script
             scraperProcess.stdout.on('data', (data) => {
                 console.log(`stdout: ${data}`);
             });
@@ -102,38 +105,38 @@ class App {
             });
         });
 
-        cron.schedule('30 22 * * *', () => {
-            const lockFilePath = './EventData.lock';
+        // cron.schedule('30 22 * * *', () => {
+        //     const lockFilePath = './EventData.lock';
 
-            // Check if lock file exists
-            if (fs.existsSync(lockFilePath)) {
-                console.log('Scraper already running. Skipping execution.');
-                return;
-            }
+        //     // Check if lock file exists
+        //     if (fs.existsSync(lockFilePath)) {
+        //         console.log('Scraper already running. Skipping execution.');
+        //         return;
+        //     }
 
-            // Create lock file
-            fs.writeFileSync(lockFilePath, '');
+        //     // Create lock file
+        //     fs.writeFileSync(lockFilePath, '');
 
-            console.log('Running EventData.js script...');
-            // Execute the results_basketball.js script using child_process.spawn
-            const scraperProcess = childProcess.spawn('node', ['./lib/cron/EventData.js']);
+        //     console.log('Running EventData.js script...');
+        //     // Execute the EventData.js script using child_process.spawn
+        //     const scraperProcess = childProcess.spawn('node', ['./lib/cron/EventData.js']);
 
-            // Log stdout and stderr from the results_basketball.js script
-            scraperProcess.stdout.on('data', (data) => {
-                console.log(`stdout: ${data}`);
-            });
+        //     // Log stdout and stderr from the EventData.js script
+        //     scraperProcess.stdout.on('data', (data) => {
+        //         console.log(`stdout: ${data}`);
+        //     });
 
-            scraperProcess.stderr.on('data', (data) => {
-                console.error(`stderr: ${data}`);
-            });
+        //     scraperProcess.stderr.on('data', (data) => {
+        //         console.error(`stderr: ${data}`);
+        //     });
 
-            // Handle script exit
-            scraperProcess.on('close', (code) => {
-                console.log(`EventData.js script exited with code ${code}`);
-                // Remove lock file
-                fs.unlinkSync(lockFilePath);
-            });
-        });
+        //     // Handle script exit
+        //     scraperProcess.on('close', (code) => {
+        //         console.log(`EventData.js script exited with code ${code}`);
+        //         // Remove lock file
+        //         fs.unlinkSync(lockFilePath);
+        //     });
+        // });
 
         cron.schedule('0 21 * * *', () => {
             const lockFilePath = './deleteData.lock';
@@ -148,10 +151,10 @@ class App {
             fs.writeFileSync(lockFilePath, '');
 
             console.log('Running deleteData.js script...');
-            // Execute the results_basketball.js script using child_process.spawn
+            // Execute the deleteData.js script using child_process.spawn
             const scraperProcess = childProcess.spawn('node', ['./lib/cron/deleteData.js']);
 
-            // Log stdout and stderr from the results_basketball.js script
+            // Log stdout and stderr from the deleteData.js script
             scraperProcess.stdout.on('data', (data) => {
                 console.log(`stdout: ${data}`);
             });
